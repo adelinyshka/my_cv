b0d73d98fea03481f09f29d5085ee042163fc181
Adel Dubinnikova <adelinyshka@gmail.com>
2019-05-01 23:36:46 +0300

    make cv with bem, pug, sass, using webpack

---
+++ src/blocks/projects/projects__slider.js
@@ -0,0 +1,53 @@
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+import 'slick-carousel';
+import 'slick-carousel/slick/slick.scss';
+import 'bootstrap'
+
+$('.responsive').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 1160,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+
+        {
+            breakpoint: 990,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 665,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
+
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+import './blocks/projects/projects__slider.js'
+import 'jquery'
+import 'slick-carousel'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import 'bootstrap'
+import './scss/_custom.scss'
+import './scss/_slickfix.scss'
+import './scss/_custom.scss'
+import './scss/_slickfix.scss'
+import './blocks/template/template.css'
+import './blocks/contacts/contacts.scss'
